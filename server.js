c6faf6cad09f1c72f3f30253555b518b0f5ae930
frogmanjhb <seancreamer62@gmail.com>
2025-10-16 20:12:18 +0200

    Simplify build process for Railway

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const { Pool } = require('pg')
 const { v4: uuidv4 } = require('uuid')
 const { createServer } = require('http')
